f0d061cf6d836f0e5c66c9e942e80b2825b634de
Huy20102002 <huy.dev.2010@gmail.com>
2022-01-14 22:26:35 +0700

    fixbanner

--- src/components/banner.js
+++ src/components/banner.js
@@ -2,7 +2,7 @@ const Banner = {
     render() {
         return `
         <a href="">
-        <img class="rounded" src="./src/image/bannernikw.png" alt="">
+        <img class="rounded" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/d8a7ed99184077.5eecde766c3bd.png" alt="">
     </a>
         `;
     },
