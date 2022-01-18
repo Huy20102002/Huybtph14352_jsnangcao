7c9bf783183afad69ce5acf74dce6a69e7653204
Huy20102002 <huy.dev.2010@gmail.com>
2022-01-18 18:19:24 +0700

    daycode

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,5 +15,10 @@ module.exports = {
             desktop: "1280px",
             // => @media (min-width: 1280px) { ... }
         },
+        extend: {
+            height: {
+                600: "34rem",
+            },
+        },
     },
 };
