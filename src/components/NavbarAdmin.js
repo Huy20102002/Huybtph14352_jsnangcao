30b60d99d8e74fd8958188096d56bbccaaed84c5
Huy20102002 <huy.dev.2010@gmail.com>
2022-01-18 17:56:20 +0700

    fixnavbar

--- src/components/NavbarAdmin.js
+++ src/components/NavbarAdmin.js
@@ -1,27 +1,54 @@
 const NavbarAdmin = {
     render() {
         return /* html */ `
-        <nav x-data="{show:false}" class="flex items-center justify-between flex-wrap bg-gray-800 p-6  ">
-        <div class="flex items-center flex-shrink-0 text-white mr-6 ">
-          <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-          <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+        <nav class="relative w-full flex flex-wrap items-center justify-between py-3 bg-white-900 text-slate-800 shadow-lg navbar navbar-expand-lg navbar-light">
+        
+        <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+            <button class="flex  navbar-toggler text-black-200 border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent1"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+        >
+          <path
+            fill="currentColor"
+            d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+          ></path>
+        </svg>
+        <a class="ml-72 pr-2" href="#">
+        <img class="w-20  lg:w-12" src="https://rubee.com.vn/admin/webroot/upload/image/images/nike-logo.jpg" alt="">
+        </a>
+      </button>
+            <div class="collapse navbar-collapse container m-auto flex-grow items-center" id="navbarSupportedContent1">
+            <div class="pr-2 ">
+            <a class=" sm:hidden md:hidden lg:block xl:block logo" href="#">
+                <img class="w-20 " src="https://rubee.com.vn/admin/webroot/upload/image/images/nike-logo.jpg" alt="">
+            </a>
+            </div>
+                <!-- Left links -->
+                <ul class="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
+                    <li class="nav-item p-2">
+                        <a class="nav-link text-black font-semibold" href="/admin/dashboard">Thống Kê</a>
+                    </li>
+                    <li class="nav-item p-2">
+                        <a class="nav-link text-black  font-semibold" href="/admin/news">Quản Lý Tin Tức</a
+                        >
+                    </li>
+                    
+                    <li class="nav-item p-2">
+                        <a  class="nav-link text-black  font-semibold" href="/admin/news/add">Quản Lý Danh Mục</a>
+                    </li>
+                    <li class="nav-item p-2">
+                        <a class="nav-link text-black text-black font-semibold" href="#">Quản Lý Sản Phẩm</a
+                        >
+                    </li>
+                    <li class="nav-item p-2">
+                        <a class="nav-link text-black text-black font-semibold" href="#">Quản Lý Bình Luận</a>
+                    </li>
+                </ul>
+            </div>
+
         </div>
-        <div class=" block md:hidden">
-          <button @click="show=!show" class=" flex items-center px-3 py-2 border rounded text-gray-100 border-gray-200 hover:text-white hover:border-white">
-            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-          </button>
-        </div>
-        <div @click.away="show = false" :class=" { 'block': show, 'hidden': !show }" class=" w-full block flex-grow md:flex md:justify-end md:w-auto">
-          <div class="">
-          <a href="/admin/dashboard" class=" block md:inline-block text-sm px-4 py-2 leading-none rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 md:mt-0" aria-current="page">Trang chủ</a>
-          <a href="/admin/news" class=" block md:inline-block text-sm px-4 py-2 leading-none rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 md:mt-0">Quản lý tin tức</a>
-          <a href="#" class=" block md:inline-block text-sm px-4 py-2 leading-none rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 md:mt-0">Quản lý danh mục</a>
-          <a href="#" class=" block md:inline-block text-sm px-4 py-2 leading-none rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 md:mt-0">Quản lý sản phẩm</a>
-          <a href="#" class=" block md:inline-block text-sm px-4 py-2 leading-none rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 md:mt-0">Quản lý bình luận</a>
-          <a href="#" class=" block md:inline-block text-sm px-4 py-2 leading-none rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 md:mt-0">Thống kê</a>
-          </div>
-        </div>
-      </nav>
+        
+    </nav>
         `;
     },
 };
